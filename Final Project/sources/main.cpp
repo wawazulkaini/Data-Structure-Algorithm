f62b351198b27ff6ce08645a6ba8bb5694efd9f2
wawazulkaini <nayliadwa@gmail.com>
2025-07-22 22:22:48 +0800

    Add files via upload

---
+++ Final Project/sources/main.cpp
@@ -0,0 +1,28 @@
+#include "mainwindow.h"
+
+#include <QApplication>
+#include <QLocale>
+#include <QTranslator>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    QTranslator translator;
+    const QStringList uiLanguages = QLocale::system().uiLanguages();
+    for (const QString &locale : uiLanguages) {
+        const QString baseName = "MergeSort_" + QLocale(locale).name();
+        if (translator.load(":/i18n/" + baseName)) {
+            a.installTranslator(&translator);
+            break;
+        }
+    }
+    MainWindow w;
+    w.show();
+    return a.exec();
+}
+
+
+/*main.cpp is entry point for MergeSort app : when running, it start executing from here
+ when adding new form or object in UI no need to edit anything from here
+ kalau rename nama mainwindow je kene edit kt sini*/
